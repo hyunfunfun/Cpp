f562120fbdb8a482b911888ea72063998ef729da
hyunfunfun(gram) <hyunfunfun@naver.com>
2024-01-26 21:45:41 +0900

    함수와 구조체(주소로 매개변수 전달)

--- cpp_study/cpp_study/func-struct.cpp
+++ cpp_study/cpp_study/func-struct.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//값으로 매개변수 전달
+//주소로 매개변수 전달
 struct Time
 {
 	int hours;
@@ -11,30 +11,30 @@ struct Time
 
 const int minsPerHr = 60;
 
-Time sum(Time, Time);
-void showTime(Time);
+Time sum(Time*, Time*);
+void showTime(Time*);
 
 int main() {
 	Time day1 = { 5,45 };
 	Time day2 = { 4,55 };
 
-	Time total = sum(day1, day2);
+	Time total = sum(&day1, &day2);
 
 	cout << "이틀간 소요 시간: ";
-	showTime(total);
+	showTime(&total);
 
 	return 0;
 }
 
-Time sum(Time t1, Time t2) {
+Time sum(Time *t1, Time *t2) {
 	Time total;
 
-	total.mins = (t1.mins + t2.mins) % minsPerHr;
-	total.hours = t1.hours + t2.hours + (t1.mins + t2.mins) / minsPerHr;
+	total.mins = (t1->mins + t2->mins) % minsPerHr;
+	total.hours = t1->hours + t2->hours + (t1->mins + t2->mins) / minsPerHr;
 
 	return total;
 }
 
-void showTime(Time t1) {
-	cout << t1.hours << "시간, " << t1.mins << "분 입니다." << endl;
+void showTime(Time *t1) {
+	cout << t1->hours << "시간, " << t1->mins << "분 입니다." << endl;
 }

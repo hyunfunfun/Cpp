fda04225fa600e2172c9ffff94304329d36989ef
hyunfunfun(gram) <hyunfunfun@naver.com>
2024-01-28 17:31:18 +0900

    디폴트 매개변수(next)

--- cpp_study/cpp_study/func4-defalt parameter.cpp
+++ cpp_study/cpp_study/func4-defalt parameter.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 const int SIZE = 8;
-int sumArr(int*, int);
+int sumArr(int*, int n=1);//디폴트값
 
 int main() {
 	int arr[SIZE] = { 1,2,4,5,16,32,64 };
-	int sum = sumArr(arr, SIZE);
+	int sum = sumArr(arr); //인자값 하나를 전달하지 않았을떄
 
 	cout << "함수의 총 합은 " << sum << " 입니다";
 	return 0;

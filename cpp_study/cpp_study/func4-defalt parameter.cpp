b0296672d2cb69c33dc091c40d073da59b105dbb
hyunfunfun(gram) <hyunfunfun@naver.com>
2024-01-28 17:29:11 +0900

    디폴트 매개변수(prev)

---
+++ cpp_study/cpp_study/func4-defalt parameter.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+const int SIZE = 8;
+int sumArr(int*, int);
+
+int main() {
+	int arr[SIZE] = { 1,2,4,5,16,32,64 };
+	int sum = sumArr(arr, SIZE);
+
+	cout << "함수의 총 합은 " << sum << " 입니다";
+	return 0;
+}
+
+int sumArr(int* arr, int n) {
+	int total = 0;
+
+	for (int i = 0; i < n; i++) {
+		total += arr[i];
+	}
+
+	return total;
+}
